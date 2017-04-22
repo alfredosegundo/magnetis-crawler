b369680e3dcd2eee70dfb49af8bd7b889d4f5058
Alfredo Segundo <alfredocavalcanti@gmail.com>
2017-04-22 13:50:35 -0300

    Read json into objects and sort by time

--- application.go
+++ application.go
@@ -1,60 +1,83 @@
 package main
 
-import "golang.org/x/net/html"
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
+
 	"net/http"
-	"strings"
+	"net/http/cookiejar"
+	"net/url"
+
+	"io/ioutil"
+
+	"os"
+
+	"encoding/json"
+
+	"sort"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func findElement(n *html.Node, tag string, tagIdKey string, tagIdValue string, attrName string) (token string, err error) {
-	if n.Type == html.ElementNode && n.Data == tag {
-		for _, a := range n.Attr {
-			if a.Key == tagIdKey && a.Val == tagIdValue {
-				for _, a := range n.Attr {
-					if a.Key == attrName {
-						return a.Val, nil
-					}
-				}
-			}
-		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		token, err := findElement(c, tag, tagIdKey, tagIdValue, attrName)
-		if err == nil {
-			return token, err
-		}
-	}
-	return "", errors.New("couldnt find token")
+var host string = "https://magnetis.com.br"
+var signin string = host + "/users/sign_in"
+var equityCurve string = host + "/pricing/api/portfolio/20146/equity_curve"
+var username string = os.Getenv("MAGNETIS_USER")
+var password string = os.Getenv("MAGNETIS_PASS")
+
+type Equity struct {
+	Time  float64
+	Value string
 }
+
+type EquityCurve struct {
+	Curve []Equity
+}
+
+func (e EquityCurve) Len() int           { return len(e.Curve) }
+func (e EquityCurve) Swap(i, j int)      { e.Curve[i], e.Curve[j] = e.Curve[j], e.Curve[i] }
+func (e EquityCurve) Less(i, j int) bool { return e.Curve[i].Time < e.Curve[j].Time }
 
 func main() {
-	content, err := getPageContent("https://magnetis.com.br/users/sign_in")
-	//content, err := ioutil.ReadFile("login.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	pageHtml := string(content)
-	doc, err := html.Parse(strings.NewReader(pageHtml))
-	if err != nil {
-		log.Fatal(err)
-	}
-	token, err := findElement(doc, "input", "name", "authenticity_token", "value")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Token: " + token)
-}
+	cookieJar, _ := cookiejar.New(nil)
 
-func getPageContent(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+	client := &http.Client{
+		Jar: cookieJar,
+	}
+	res, err := client.Get(signin)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	selection := doc.Find("input[name='authenticity_token']")
+	token, _ := selection.First().Attr("value")
+	client.PostForm(signin, url.Values{
+		"authenticity_token": {token},
+		"utf8":               {"✓"},
+		"user[email]":        {username},
+		"user[password]":     {password},
+	})
+	resp, err := client.Get(equityCurve)
 	body, err := ioutil.ReadAll(resp.Body)
-	return body, err
+
+	curve := make([][]interface{}, 0)
+
+	err = json.Unmarshal(body, &curve)
+	if err != nil {
+		fmt.Println(string(body))
+		log.Fatal(err)
+	}
+
+	equityCurve := new(EquityCurve)
+	for i := range curve {
+		value := curve[i][1]
+		equity := Equity{Time: curve[i][0].(float64), Value: value.(string)}
+		equityCurve.Curve = append(equityCurve.Curve, equity)
+	}
+
+	sort.Sort(equityCurve)
+	fmt.Println(equityCurve)
 }
