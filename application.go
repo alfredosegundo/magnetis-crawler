a6e8245ea82d8290c0385c2413530f3f57d45424
Alfredo Segundo <alfredocavalcanti@gmail.com>
2017-05-07 13:08:09 -0300

    Added github.com/urfave/cli to manage commands

--- application.go
+++ application.go
@@ -6,6 +6,8 @@ import (
 
 	"MagnetisCrawler/magnetis"
 	"os"
+
+	"github.com/urfave/cli"
 )
 
 var userId string = os.Getenv("MAGNETIS_USER_ID")
@@ -13,34 +15,98 @@ var username string = os.Getenv("MAGNETIS_USER")
 var password string = os.Getenv("MAGNETIS_PASS")
 
 func main() {
-	err := magnetis.Signin(username, password)
-	if err != nil {
-		log.Fatal(err)
+	var shouldSave bool
+	var shouldPrint bool
+	app := cli.NewApp()
+	app.Name = "Magnetis Crawler"
+	app.Usage = "Get my data form magnetis website"
+	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:        "save, s",
+			Usage:       "if we should save on drive",
+			Destination: &shouldSave,
+		},
+		cli.BoolFlag{
+			Name:        "print, p",
+			Usage:       "Print on the console",
+			Destination: &shouldPrint,
+		},
 	}
 
-	curve, err := magnetis.GetEquityCurve(userId)
-	if err != nil {
-		log.Fatal(err)
-	}
+	app.Commands = []cli.Command{
+		{
+			Name:    "curve",
+			Aliases: []string{"c"},
+			Usage:   "Get your equity curve from magnetis api",
+			Action: func(c *cli.Context) error {
+				err := magnetis.Signin(username, password)
+				if err != nil {
+					log.Fatal(err)
+				}
 
-	equities := curve.Equities
-	for i := range equities {
-		fmt.Println(equities[i])
+				curve, err := magnetis.GetEquityCurve(userId)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if shouldPrint {
+					equities := curve.Equities
+					for i := range equities {
+						fmt.Println(equities[i])
+					}
+				}
+				if shouldSave {
+					log.Fatal("Not implemented yet")
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "plan",
+			Usage: "Get your investment plan from magnetis api",
+			Action: func(c *cli.Context) error {
+				err := magnetis.Signin(username, password)
+				if err != nil {
+					log.Fatal(err)
+				}
+				plan, err := magnetis.GetInvestmentPlan(userId)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if shouldPrint {
+					fmt.Printf("%#v\n", plan)
+				}
+				if shouldSave {
+					log.Fatal("Not implemented yet")
+				}
+				return nil
+			},
+		},
+		{
+			Name:    "assets",
+			Aliases: []string{"a"},
+			Usage:   "Get your assets from magnetis api",
+			Action: func(c *cli.Context) error {
+				err := magnetis.Signin(username, password)
+				if err != nil {
+					log.Fatal(err)
+				}
+				assets, err := magnetis.Assets(userId)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if shouldPrint {
+					for i := range assets {
+						fmt.Printf("%#v\n", assets[i])
+					}
+				}
+				if shouldSave {
+					log.Fatal("Not implemented yet")
+				}
+				return nil
+			},
+		},
 	}
-
-	plan, err := magnetis.GetInvestmentPlan(userId)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(plan)
-
-	assets, err := magnetis.Assets(userId)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for i := range assets {
-		fmt.Printf("%#v\n", assets[i])
-	}
+	app.Run(os.Args)
 }
