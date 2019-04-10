8633951062bcf3d81ed20b081ad3daaabce2f5ba
Alfredo Segundo <asegundo@thoughtworks.com>
2019-04-10 15:36:14 -0300

    Improved error logging and comments

--- lambda/main.go
+++ lambda/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/alfredosegundo/magnetis-crawler/magnetis"
@@ -11,19 +10,23 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// MyEvent is the event dispatched by aws lambda infrastrucute
+// to start the function
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
+// HandleRequest is the entrypoint of the lambda function
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	err := magnetis.MagnetisSignin(os.Getenv("MAGNETIS_USER_ID"), os.Getenv("MAGNETIS_PASSWORD"))
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Sprintf("Failed to login: \n%v", err), err
 	}
 	plan, err := magnetis.GetInvestmentPlan(os.Getenv("USER_ID"))
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Sprintf("Failed to get investment plan: \n%v", err), err
 	}
+
 	return fmt.Sprintf("Plan: %v", plan), nil
 }
 
